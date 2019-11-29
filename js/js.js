18ae12e2a463eb6d72b498062f781dc0c6256cc5
tuckpuck <tucker.triggs@yahoo.com>
2019-11-29 11:57:17 -0700

    add orange animation

--- js/js.js
+++ js/js.js
@@ -22,6 +22,18 @@ let blueAnimation = blue.animate(
     delay: 100
   }
 );
+let orange = document.querySelector(".orange");
+
+let orangeAnimation = orange.animate(
+  [{ transform: "translateY(-2em)" }, { transform: "translateY(2em)" }],
+  {
+    duration: 1000,
+    iterations: Infinity,
+    direction: "alternate",
+    easing: "ease-in-out",
+    delay: 200
+  }
+);
 
 let pauseButton = document.querySelector(".pause");
 pauseButton.addEventListener("click", () => {
@@ -37,6 +49,7 @@ function togglePause() {
 function play() {
   greenAnimation.play();
   blueAnimation.play();
+  orangeAnimation.play();
   paused = false;
   pauseButton.innerText = "Pause";
 }
@@ -44,6 +57,7 @@ function play() {
 function pause() {
   greenAnimation.pause();
   blueAnimation.pause();
+  orangeAnimation.pause();
   paused = true;
   pauseButton.innerText = "Play";
 }
